ce5444e345661e34eeb0f700f15cbe070f148bf6
anthonyayokh <anthonyayod24@gmail.com>
2022-10-24 08:39:25 +0800

    This is your first commit

---
+++ Tent.py
@@ -0,0 +1,7 @@
+stationaries = ['pencil', 'eraser', 'hi' ,'d' ,'man' ,'mad' ,'ten' , 'ruler' ]
+stationaries[1] = 'pom'
+stationaries = pent
+for x in pent:
+    if x == 'pom':
+        break
+        print(pent)
